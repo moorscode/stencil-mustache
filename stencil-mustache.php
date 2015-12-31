1f460b44a034f3509a20faaa004765d46ec1cca7
jip laptop <jhp.moors@gmail.com>
2015-12-31 17:49:16 +0100

    translation and description fixes

--- stencil-mustache.php
+++ stencil-mustache.php
@@ -1,10 +1,10 @@
 <?php
 /**
 Plugin Name: Stencil: Mustache Implementation
-Plugin URI: http://www.jipmoors.nl/stencil/
-Description: Stencil Implementation. This addon enables the use of Mustache in your theme. This addon requires the plugin Stencil to be installed and activated.
+Plugin URI: https://github.com/moorscode/stencil/
+Description: Mustache Stencil Implementation. This plugin enables the use of Mustache in your theme. This implementation requires the plugin "Stencil" to be installed and active.
 Version: 1.0.0
-Author: jipmoors
+Author: Jip Moors (moorscode)
 Author URI: http://www.jipmoors.nl
 Text Domain: stencil
 License: GPL2
